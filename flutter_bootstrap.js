9ef75664a9ed6b5e9653fbd5aed7506b30fb63a5
unknown <amittiwari.scholarhat@gmail.com>
2026-08-09 13:05:14 +0530

    Update Play Pause logic

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1509326162" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "647449594" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
