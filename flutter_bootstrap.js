f4f69d4322375c8bfe2e07fe33e7346a89b30787
unknown <amittiwari.scholarhat@gmail.com>
2026-08-09 13:24:46 +0530

    Fix import for tuner provider

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1784821996" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3567427715" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
