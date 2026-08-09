9226b43e5bcdb8e64771118bff1e05f0c58f4605
unknown <amittiwari.scholarhat@gmail.com>
2026-08-09 13:47:12 +0530

    Fix meditation screen and bad import

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "926695268" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3589862969" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
